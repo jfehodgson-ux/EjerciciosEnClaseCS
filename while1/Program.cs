82e4e1839ab5b016af2ab74828ce1ad501a89842
Jonas <jfehodgson@uamv.edu.ni>
2026-05-04 12:17:17 -0600

    Ejercicio While1

---
+++ while1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Leer un numero entero y mostrar el cuadrado de sus antecedores
+
+using System;
+
+int numero;
+
+while (true)
+{
+    Console.Write("Ingrese un numero entero: ");
+    if (int.TryParse(Console.ReadLine(), out numero))
+    {
+        break;
+    }
+    else
+    {   
+        Console.ForegroundColor = ConsoleColor .Red;
+        Console.WriteLine("Entrada no valida. Por favor, ingrese un numero entero.");
+        Console.ResetColor();
+
+    }
+}
+Console.WriteLine("Mostrando el cuadrado de los antecesores de " + numero);
+
+int cont= 1;
+while (cont <= numero)
+{
+
+    Console.WriteLine("El cuadrado de " + cont + " es: " + Math.Pow(cont, 2));
+    cont++;
+}

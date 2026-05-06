f4a672870adf8a181f3035d720c832e369cbef99
Jonas <jfehodgson@uamv.edu.ni>
2026-05-06 12:24:40 -0600

    fix agregue validaciones

--- arreglo1/Program.cs
+++ arreglo1/Program.cs
@@ -8,6 +8,16 @@ double promedio;
 
 for (int i = 0; i < edades.Length; i++)
 {
+    try
+    {
+         Console.WriteLine("Ingrese la edad " + (i + 1) + ":");
+         edades[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un Decrementar el índice para volver a solicitar la edadnúmero entero.");
+            i--; // 
+    }
     Console.WriteLine("Ingrese la edad " + (i + 1) + ":");
     edades[i] = int.Parse(Console.ReadLine()!);
     

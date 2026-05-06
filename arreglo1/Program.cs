b1576f5f972a9c9194725247c0f810c341ae0e98
Jonas <jfehodgson@uamv.edu.ni>
2026-05-06 12:04:47 -0600

    feat: add arreglo1 project

---
+++ arreglo1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Leer 5 edades y mostrar la edad promedio 
+
+using System;
+
+int[] edades = new int[5];
+int suma = 0;
+double promedio;
+
+for (int i = 0; i < edades.Length; i++)
+{
+    Console.WriteLine("Ingrese la edad " + (i + 1) + ":");
+    edades[i] = int.Parse(Console.ReadLine()!);
+    
+}
+foreach (int edad in edades)
+{
+    suma += edad;
+}
+promedio = (double)suma / edades.Length;
+
+Console.WriteLine("La edad promedio es: " + promedio);

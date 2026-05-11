8b2f196b15237d8f93c84e06b38cd345ba6aafb0
Jonas <jfehodgson@uamv.edu.ni>
2026-05-10 21:26:51 -0600

    feat: agregar ejercicio de resta de vectores

---
+++ RestaVectores/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+
+int[] vector1 = new int [5];
+int[] vector2 = new int [5];
+int[] restaVector = new int [5];
+
+Console.WriteLine("Ingrese los elementos del primer vector:");
+for (int i = 0; i < vector1.Length; i++)
+{
+    try
+    {
+        Console.Write($"Elemento {i + 1}: ");
+        vector1[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Entrada inválida. Por favor, ingrese un número entero.");
+        i--; // Decrementar el indicee para volver a solicitar el mismo elemento
+        Console.ResetColor();
+    }
+}
+
+Console.WriteLine("Ingrese los elementos del segundo vector:");
+
+for(int i = 0; i < vector2.Length; i++)
+{
+    try
+    {
+        Console.Write($"Elemento {i + 1}: ");
+        vector2[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Entrada inválida. Por favor, ingrese un número entero.");
+        i--; // Decrementar el indicee para volver a solicitar el mismo elemento
+        Console.ResetColor();
+    }
+}
+
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("La resta de los dos vectores es:");
+for (int i = 0; i < restaVector.Length; i++)
+{
+    restaVector[i] = vector1[i] - vector2[i];
+}
+
+Console.ResetColor();
+for (int i = 0; i < restaVector.Length; i++)
+{
+    Console.WriteLine($"Elemento {i + 1}: {restaVector[i]}");
+}
